7aa831362a73d609fcfdaff4e0f253c54fac513a
diane2007 <wangruochong0406@gmail.com>
2023-03-19 15:04:17 -0400

    Score system is still broken but otherwise working well. I don't know how to fix the score system...

--- rw2324-S23-CodeLab1-Week6/Assets/Scripts/GameManager.cs
+++ rw2324-S23-CodeLab1-Week6/Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -7,8 +8,7 @@ public class GameManager : MonoBehaviour
 {
     //create a singleton and attach it to GameManagerHolder
     public static GameManager instance;
-    
-    
+    public TextMeshProUGUI scoreText;
 
     void Awake()
     {
@@ -22,6 +22,24 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+        
+    //create a box for scores, making it a property
+    int score = 0;
+    //bools to make sure ChangeScore() only calls once
+    bool scoreChanged = false;
+    public int Score
+    {
+        get { return score; }
+        set
+        {
+            //reset scoreChanged
+            //scoreChanged = false;
+            score = value;
+            //whenever score changes, change scoreText
+            ChangeScore(); 
+            
+        }
+    }
     
     //create a container for player's input answer
     [FormerlySerializedAs("answer")] public string playerAnswer;
@@ -31,8 +49,19 @@ public class GameManager : MonoBehaviour
     public bool isAnswered = false;
     public bool isCorrect = false;
     public bool isWrong = false;
-    
-    //public List<string> playerAnswer;
+
+    void Start()
+    {
+        //show the score text
+        ChangeScore();
+    }
+
+    void ChangeScore()
+    {
+        scoreText.text = "Your score:" + "\n" + Score;
+        isAnswered = false;
+        Debug.Log("After changing score, isAnswered is " + isAnswered);
+    }
 
     void Update()
     {
@@ -40,14 +69,27 @@ public class GameManager : MonoBehaviour
         {
             if (correctAnswer == playerAnswer)
             {
-                isCorrect = true;
                 Debug.Log("Correct!");
+                Debug.Log("score changed bool is: " + scoreChanged.ToString() + " isAnswered is: " + isAnswered.ToString());
+                isCorrect = true;
+                if(scoreChanged == false)
+                {
+                    Score++;    //score +1 when correct
+                    scoreChanged = true;
+                }
             }
             else
             {
-                isWrong = true;
                 Debug.Log("Wrong!");
+                Debug.Log("score changed bool is: " + scoreChanged.ToString() + "isAnswered is: " + isAnswered.ToString());
+                isWrong = true;
+                if(scoreChanged == false)
+                {
+                    Score--;    //score -1 when wrong
+                    scoreChanged = true;
+                }
             }
+
         }
 
     }
